133b1cbed0886822dc794df39d14c9ca0db997c4
Luigi <luigiaubrypouget@gmail.com>
2026-03-30 14:55:30 +0200

    add Editeur, Genre and Jeu class

--- src/main/java/fr/esgi/avis/business/Jeu.java
+++ src/main/java/fr/esgi/avis/business/Jeu.java
@@ -1,21 +1,16 @@
 package fr.esgi.avis.business;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Jeu {
-
-    public Jeu(Genre genre, String nom, List<Plateforme> plateformes, Editeur editeur, LocalDate dateDeSortie, String description, float prix, Classification classification, String image) {
-        this.genre = genre;
-        this.nom = nom;
-        this.plateformes = plateformes;
-        this.editeur = editeur;
-        this.dateDeSortie = dateDeSortie;
-        this.description = description;
-        this.prix = prix;
-        this.classification = classification;
-        this.image = image;
-    }
 
     private List<Plateforme> plateformes;
 
@@ -36,76 +31,4 @@ public class Jeu {
     private Classification classification;
 
     private String image;
-
-    public List<Plateforme> getPlateformes() {
-        return plateformes;
-    }
-
-    public void setPlateformes(List<Plateforme> plateformes) {
-        this.plateformes = plateformes;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public Editeur getEditeur() {
-        return editeur;
-    }
-
-    public void setEditeur(Editeur editeur) {
-        this.editeur = editeur;
-    }
-
-    public LocalDate getDateDeSortie() {
-        return dateDeSortie;
-    }
-
-    public void setDateDeSortie(LocalDate dateDeSortie) {
-        this.dateDeSortie = dateDeSortie;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public float getPrix() {
-        return prix;
-    }
-
-    public void setPrix(float prix) {
-        this.prix = prix;
-    }
-
-    public Classification getClassification() {
-        return classification;
-    }
-
-    public void setClassification(Classification classification) {
-        this.classification = classification;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 }
--- src/main/java/fr/esgi/avis/business/Plateforme.java
+++ src/main/java/fr/esgi/avis/business/Plateforme.java
@@ -1,14 +1,16 @@
 package fr.esgi.avis.business;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Plateforme {
-
-    public Plateforme(String nom, LocalDate dateDeSortie) {
-        this.nom = nom;
-        this.dateDeSortie = dateDeSortie;
-    }
 
     private Long id;
 
@@ -17,28 +19,4 @@ public class Plateforme {
     private List<Jeu> jeux;
 
     private LocalDate dateDeSortie;
-
-    public List<Jeu> getJeux() {
-        return jeux;
-    }
-
-    public void setJeux(List<Jeu> jeux) {
-        this.jeux = jeux;
-    }
-
-    public LocalDate getDateDeSortie() {
-        return dateDeSortie;
-    }
-
-    public void setDateDeSortie(LocalDate dateDeSortie) {
-        this.dateDeSortie = dateDeSortie;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
 }
