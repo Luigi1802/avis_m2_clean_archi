615427dfaad18cad8f843161f5718e4c4414a297
Luigi <luigiaubrypouget@gmail.com>
2026-03-30 18:30:37 +0200

    add entity

---
+++ src/main/java/fr/esgi/avis/entity/EditeurEntity.java
@@ -0,0 +1,32 @@
+package fr.esgi.avis.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import fr.esgi.avis.business.Jeu;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class EditeurEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank(message="Merci de préciser le nom de l'éditeur")
+    @Size(min=2, message="Le nom de l'éditeur doit comporter au moins {min} caractères")
+    @Column(name = "nom", nullable = false, unique = true)
+    private String nom;
+
+    @OneToMany(mappedBy="editeur")
+    @ToString.Exclude
+    private List<JeuEntity> jeux;
+}
