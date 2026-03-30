6b5a0d4c8d460fc1c998d0c25be884ebe66d3501
Luigi <luigiaubrypouget@gmail.com>
2026-03-30 14:23:41 +0200

    add Plateforme class

---
+++ src/main/java/fr/esgi/avis/business/Plateforme.java
@@ -0,0 +1,14 @@
+package fr.esgi.avis.business;
+
+import java.time.LocalDate;
+
+public class Plateforme {
+
+    private Long id;
+
+    private String nom;
+
+    private List<Jeu> jeux;
+
+    private LocalDate dateDeSortie;
+}
