fbe3ecc6b9e293e68d331b924f5f035478f08dfc
pho-rg <phdorglandes@hotmail.fr>
2026-04-28 17:57:30 +0200

    jwt OK

---
+++ src/main/java/fr/esgi/avis/adapter/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package fr.esgi.avis.adapter.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Avis API")
+                        .version("1.0")
+                        .description("API de gestion des avis de jeux")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .name("Bearer Authentication")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
