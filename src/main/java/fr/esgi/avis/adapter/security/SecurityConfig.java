f943118235cd3e730d11c8ea237afd24a5abc035
pho-rg <phdorglandes@hotmail.fr>
2026-04-28 16:28:19 +0200

    clear swagger routes

--- src/main/java/fr/esgi/avis/adapter/security/SecurityConfig.java
+++ src/main/java/fr/esgi/avis/adapter/security/SecurityConfig.java
@@ -29,6 +29,15 @@ public class SecurityConfig {
 
                         .requestMatchers("/api/auth/**").permitAll()
 
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
